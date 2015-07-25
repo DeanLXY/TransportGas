bbcd740119880586abdeca309136e86f8a054dfe
wangxujie <wangxj0714@163.com>
2015-07-25 17:11:41 +0800

    dbhelper

--- app/src/main/java/com/android/db/DbHelper.java
+++ app/src/main/java/com/android/db/DbHelper.java
@@ -1,43 +1,65 @@
 package com.android.db;
 
-import com.android.Constants;
-
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.android.Constants;
+import com.android.module.Information;
+
 public class DbHelper {
-	private DbOpenHelper helper;
-	private static DbHelper mHelper = null;
-	private DbHelper(Context context) {
-		helper = new DbOpenHelper(context, Constants.DBNAME);
-	}
+    private DbOpenHelper helper;
+    private static DbHelper mHelper = null;
 
-	
-	public static DbHelper getInstance(Context context) {
-		if (mHelper == null) {
-			synchronized (DbHelper.class) {
-				if (mHelper == null)
-					mHelper = new DbHelper(context);
-			}
-		}
-		return mHelper;
-	}
+    private DbHelper(Context context) {
+        helper = new DbOpenHelper(context, Constants.DBNAME);
+    }
 
-	private class DbOpenHelper extends SQLiteOpenHelper {
 
-		public DbOpenHelper(Context context, String name) {
-			super(context, name, null, 1);
-		}
+    public static DbHelper getInstance(Context context) {
+        if (mHelper == null) {
+            synchronized (DbHelper.class) {
+                if (mHelper == null)
+                    mHelper = new DbHelper(context);
+            }
+        }
+        return mHelper;
+    }
 
-		@Override
-		public void onCreate(SQLiteDatabase db) {
+    public void saveOrUpdateInfomation(Information information) {
+        SQLiteDatabase database = helper.getWritableDatabase();
+        Cursor cursor = database.rawQuery("Select * from orderInfo where orderNum=? ", new String[]{information.getOrderNum()});
+        if(cursor.moveToFirst()){
+            //exists
+        }
+    }
 
-		}
+    private class DbOpenHelper extends SQLiteOpenHelper {
 
-		@Override
-		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        public DbOpenHelper(Context context, String name) {
+            super(context, name, null, 1);
+        }
 
-		}
-	}
+        private final String ORDERTABLECREATESQL = "CREATE TABLE orderInfo IF NOT EXISTS(_id integer primary key autoincrement," +//
+                "orderNum varchar(100)," +//
+                "orderAddress varchar(200) NOT NULL," +//
+                "orderContacts varchar(200) NOT NULL," +//
+                "orderPhoneNum varchar(200) NOT NULL," +//
+                "orderTime varchar(200) NOT NULL," +//
+                "orderContent varchar(200) NOT NULL" +//
+                ")";
+
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(ORDERTABLECREATESQL);
+            db.execSQL("CREATE INDEX orderIndex ON orderInfo (orderNum)");
+
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        }
+    }
 }
--- app/src/main/java/com/android/transport/TransportMain.java
+++ app/src/main/java/com/android/transport/TransportMain.java
@@ -2,6 +2,9 @@ package com.android.transport;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -13,87 +16,97 @@ import com.example.transportgas.R;
 @ContentView(R.layout.transport_main)
 public class TransportMain extends IActivity implements OnClickListener {
 
-	@ViewId(R.id.orders)
-	private Button mOrders; // 配送接单
-	@ViewId(R.id.navigation)
-	private Button mNavigation;// 配送导航
-	@ViewId(R.id.transfer)
-	private Button mTransfer; // 配送交接
-	@ViewId(R.id.pay)
-	private Button mPay; // 确认支付
-	@ViewId(R.id.track)
-	private Button mTrack; // 气瓶跟踪
-	@ViewId(R.id.sales)
-	private Button mSales; // 促销活动
-	@ViewId(R.id.service)
-	private Button mService; // 服务评价
-	@ViewId(R.id.analysis)
-	private Button mAnalysis; // 配销分析
-	@ViewId(R.id.btn_setting)
-	private View mSetting; // 系统设置
+    @ViewId(R.id.orders)
+    private Button mOrders; // 配送接单
+    @ViewId(R.id.navigation)
+    private Button mNavigation;// 配送导航
+    @ViewId(R.id.transfer)
+    private Button mTransfer; // 配送交接
+    @ViewId(R.id.pay)
+    private Button mPay; // 确认支付
+    @ViewId(R.id.track)
+    private Button mTrack; // 气瓶跟踪
+    @ViewId(R.id.sales)
+    private Button mSales; // 促销活动
+    @ViewId(R.id.service)
+    private Button mService; // 服务评价
+    @ViewId(R.id.analysis)
+    private Button mAnalysis; // 配销分析
+    @ViewId(R.id.btn_setting)
+    private View mSetting; // 系统设置
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		mOrders.setOnClickListener(this);
-		mNavigation.setOnClickListener(this);
-		mTransfer.setOnClickListener(this);
-		mPay.setOnClickListener(this);
-		mTrack.setOnClickListener(this);
-		mSales.setOnClickListener(this);
-		mService.setOnClickListener(this);
-		mAnalysis.setOnClickListener(this);
-		mSetting.setOnClickListener(this);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mOrders.setOnClickListener(this);
+        mNavigation.setOnClickListener(this);
+        mTransfer.setOnClickListener(this);
+        mPay.setOnClickListener(this);
+        mTrack.setOnClickListener(this);
+        mSales.setOnClickListener(this);
+        mService.setOnClickListener(this);
+        mAnalysis.setOnClickListener(this);
+        mSetting.setOnClickListener(this);
+    }
 
-	@Override
-	public void onBackPressed() {
-		super.onBackPressed();
-		moveTaskToBack(true);
-	}
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        moveTaskToBack(true);
+    }
 
-	@Override
-	public void onClick(View v) {
-		Intent intent;
-		switch (v.getId()) {
-		case R.id.orders:
-			intent = new Intent(this, TransportOrder.class);
-			startActivity(intent);
-			break;
-		case R.id.navigation:
-			intent = new Intent(this, TransportNavigation.class);
-			startActivity(intent);
-			break;
-		case R.id.transfer:
-			intent = new Intent(this, TransportTransfer.class);
-			startActivity(intent);
-			break;
-		case R.id.pay:
-			intent = new Intent(this, TransportPay.class);
-			startActivity(intent);
-			break;
-		case R.id.track:
-			intent = new Intent(this, TransportTrack.class);
-			startActivity(intent);
-			break;
-		case R.id.sales:
-			intent = new Intent(this, TransportSales.class);
-			startActivity(intent);
-			break;
-		case R.id.service:
-			intent = new Intent(this, TransportService.class);
-			startActivity(intent);
-			break;
-		case R.id.analysis:
-			intent = new Intent(this, TransportAnalysis.class);
-			startActivity(intent);
-			break;
-		case R.id.btn_setting:
-			intent = new Intent(this, TransportSettings.class);
-			startActivity(intent);
-			break;
-		}
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        if (v == mOrders) {
+            intent = new Intent(this, TransportOrder.class);
+            startActivity(intent);
+        } else if (v == mNavigation) {
+            intent = new Intent(this, TransportNavigation.class);
+            startActivity(intent);
+        } else if (v == mTransfer) {
+            intent = new Intent(this, TransportTransfer.class);
+            startActivity(intent);
+        } else if (v == mPay) {
+            intent = new Intent(this, TransportPay.class);
+            startActivity(intent);
+        } else if (v == mTrack) {
+            intent = new Intent(this, TransportTrack.class);
+            startActivity(intent);
+        } else if (v == mSales) {
+            intent = new Intent(this, TransportSales.class);
+            startActivity(intent);
+        } else if (v == mService) {
+            intent = new Intent(this, TransportService.class);
+            startActivity(intent);
+        } else if (v == mAnalysis) {
+            intent = new Intent(this, TransportAnalysis.class);
+            startActivity(intent);
+        } else if (v == mSetting) {
+            showSettingOptions(v);
+        }
+    }
 
-	}
+
+    private void showSettingOptions(View view) {
+        PopupMenu popup = new PopupMenu(this, view);
+        popup.getMenu().add(Menu.NONE, 0, Menu.NONE, "设备注册");
+        popup.getMenu().add(Menu.NONE, 1, Menu.NONE, "系统设置");
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                if (id == 0) {
+                    Intent intent = new Intent(TransportMain.this, TransportRegister.class);
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(TransportMain.this, TransportSettings.class);
+                    startActivity(intent);
+                }
+                return true;
+            }
+        });
+        popup.show(); //showing popup menu
+    }
 
 }
