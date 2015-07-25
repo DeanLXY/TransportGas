00f82dab0b71397345c40fa93a1695f1c3293213
wangxujie <wangxj0714@163.com>
2015-07-25 16:55:48 +0800

    Information

--- app/src/main/java/com/android/module/Information.java
+++ app/src/main/java/com/android/module/Information.java
@@ -1,107 +1,81 @@
 package com.android.module;
 
+import android.support.annotation.NonNull;
+
 /**
  * @author Administrator
- *
  */
-public class Information implements Comparable<Information>{
-    private int id;
-    private String address;
-    private String order;
-    private String time;
-    private String consumer;
-    private String telephony;
-    public String getAddress() {
-		return address;
-	}
+public class Information implements Comparable<Information> {
+    private String orderNum;
+    private String orderAddress;
+    private String orderContacts;
+    private String orderPhoneNum;
+    private String orderTime;
+    private String orderContent;
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getOrder() {
-		return order;
-	}
-
-	public void setOrder(String order) {
-		this.order = order;
-	}
-
-	public String getTime() {
-		return time;
-	}
-
-	public void setTime(String time) {
-		this.time = time;
-	}
-
-	public String getConsumer() {
-		return consumer;
-	}
-
-	public void setConsumer(String consumer) {
-		this.consumer = consumer;
-	}
-
-	public String getTelephony() {
-		return telephony;
-	}
-
-	public void setTelephony(String telephony) {
-		this.telephony = telephony;
-	}
-
-	public Information() {
-	}
-
-    public Information(int id, String address, String order, String time, String consumer, String telephony){
-    	this.id = id;
-    	this.address = address;
-    	this.order = order;
-    	this.time = time;
-    	this.consumer = consumer;
-    	this.telephony = telephony;
+    public String getOrderNum() {
+        return orderNum;
     }
-    
-	public int getId() {
-		return id;
-	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		return result;
-	}
+    public void setOrderNum(String orderNum) {
+        this.orderNum = orderNum;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Information other = (Information) obj;
-		if (id != other.id)
-			return false;
-		return true;
-	}
+    public String getOrderAddress() {
+        return orderAddress;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setOrderAddress(String orderAddress) {
+        this.orderAddress = orderAddress;
+    }
 
-	@Override
-	public String toString() {
-		return "Information [id=" + id + ", address=" + address + ", order=" + order + ", time=" + time + ", consumer="
-				+ consumer + ", telephony=" + telephony + "]";
-	}
+    public String getOrderContacts() {
+        return orderContacts;
+    }
 
-	//排序使用 距离最近在上边
-	@Override
-	public int compareTo(Information another) {
-		return 0;
-	}
+    public void setOrderContacts(String orderContacts) {
+        this.orderContacts = orderContacts;
+    }
+
+    public String getOrderPhoneNum() {
+        return orderPhoneNum;
+    }
+
+    public void setOrderPhoneNum(String orderPhoneNum) {
+        this.orderPhoneNum = orderPhoneNum;
+    }
+
+    public String getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(String orderTime) {
+        this.orderTime = orderTime;
+    }
+
+    public String getOrderContent() {
+        return orderContent;
+    }
+
+    public void setOrderContent(String orderContent) {
+        this.orderContent = orderContent;
+    }
+
+    @Override
+    public String toString() {
+        return "Information{" +
+                "orderNum='" + orderNum + '\'' +
+                ", orderAddress='" + orderAddress + '\'' +
+                ", orderContacts='" + orderContacts + '\'' +
+                ", orderPhoneNum='" + orderPhoneNum + '\'' +
+                ", orderTime='" + orderTime + '\'' +
+                ", orderContent='" + orderContent + '\'' +
+                '}';
+    }
+
+    //排序使用 距离最近在上边
+    @Override
+    public int compareTo(@NonNull Information another) {
+        return 0;
+    }
 }
